0a66363384d4fa3fa749997fa3b0a410bd9afdd7
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2020-11-09 18:06:18 +0100

    Update Target.cpp
    Added Comments

--- Utils/Target.cpp
+++ Utils/Target.cpp
@@ -23,46 +23,54 @@ bool Target::isValidTarget(C_Entity* ent) {
 
 	if (!ent->isAlive())
 		return false;
-
+	//Entity-Id check
 	if (ent->getEntityTypeId() <= 122 && ent->getEntityTypeId() != 63 && antibot->isEntityIdCheckEnabled())
 		return false;
-
+	//Teams
 	if (ent->getEntityTypeId() == 63) {
+		//Color check
 		if (teams->isColorCheckEnabled()) {
 			std::string targetName = ent->getNameTag()->getText();
 			std::string localName = g_Data.getLocalPlayer()->getNameTag()->getText();
 			if (targetName.length() > 2 && localName.length() > 2) {
 				targetName = std::regex_replace(targetName, std::regex("\\§r"), "");
 				localName = std::regex_replace(localName, std::regex("\\§r"), "");
-				if (targetName.at(0) == localName.at(0)) return false;
+				if (targetName.at(0) == localName.at(0))
+					return false;
 			}
 		}
+		//Allied check
 		if (teams->isAlliedCheckEnabled()) {
 			C_LocalPlayer* p = g_Data.getLocalPlayer();
-			if (p->isAlliedTo(ent)) return false;
+			if (p->isAlliedTo(ent))
+				return false;
 		}
 	}
 
 	// Temporarily removed from gui, tons of false negatives
 	if (!Target::containsOnlyASCII(ent->getNameTag()->getText()) && antibot->isNameCheckEnabled())
 		return false;
-
+	//Friend check
 	if (FriendList::findPlayer(ent->getNameTag()->getText()) && !moduleMgr->getModule<NoFriends>()->isEnabled())
 		return false;
-
+	//Invis Check
 	if (ent->isInvisible() && antibot->isInvisibleCheckEnabled())
 		return false;
-
-	if ((ent->isSilent() || ent->isImmobile() || ent->getNameTag()->getTextLength() < 1 || std::string(ent->getNameTag()->getText()).find(std::string("\n")) != std::string::npos) && antibot->isOtherCheckEnabled())
+	//Other Check
+	if ((ent->isSilent()
+		|| ent->isImmobile() 
+		|| ent->getNameTag()->getTextLength() < 1 
+		|| std::string(ent->getNameTag()->getText()).find(std::string("\n")) != std::string::npos)
+			&& antibot->isOtherCheckEnabled())
 		return false;
-
+	//Hitbox check
 	if (!hitboxMod->isEnabled() && antibot->isHitboxCheckEnabled())
 		if ((ent->height < 1.5f || ent->width < 0.49f || ent->height > 2.1f || ent->width > 0.9f))
 			return false;
 
 	if (!(*localPlayer)->canAttack(ent, false))
 		return false;
-
+	//Extra check
 	if (antibot->isExtraCheckEnabled() && !ent->canShowNameTag())
 		return false;
 
