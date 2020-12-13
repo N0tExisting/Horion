020a596943be0285851a1a0d6c334bae3322757a
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2020-12-13 12:59:00 +0100

    Improved Jesus
    Added  old mode as an option & player pos is now at water height.

--- Horion/Module/Modules/Jesus.h
+++ Horion/Module/Modules/Jesus.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "Module.h"
-class Jesus :
-	public IModule
-{
+class Jesus : public IModule {
 private:
 	bool wasInWater = false;
+	bool Solid = true;
+
 public:
 	Jesus();
 	~Jesus();
